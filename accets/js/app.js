98a899f340a2ca8a260721a9065585e8e9064fbb
AshwiniPatil1108 <ashubpatil1108@gmail.com>
2024-05-16 05:41:30 -0700

    First Commit

---
+++ accets/js/app.js
@@ -0,0 +1,24 @@
+const cl = console.log;
+const allAccHead = [...document.querySelectorAll(".acco-head")];
+
+const onHeadClick = (eve)=>{
+	let getClassNames = eve.target.className;
+	cl(getClassNames);
+	let activeHead = document.querySelector(".acco-head.active");
+	if(activeHead){
+		activeHead.classList.remove("active");
+	}
+	if(getClassNames.toLowerCase().includes('active')){
+		eve.target.classList.remove("active");
+	}else{
+		eve.target.classList.add("active");
+	}
+}
+
+
+
+
+allAccHead.forEach(accHead =>{
+	cl(accHead)
+	accHead.addEventListener("click", onHeadClick)
+})
